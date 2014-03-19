348cea3bdccd14c4625659d4dc2a9607de9b48d0
Lajos Molnar <lajos@google.com>
2014-03-19 15:13:24 -0700

    stagefright: use media_codecs_google*.xml files
    Bug: 13549114
    Change-Id: If93ffa7cefb6f3271bdd5ad5abf331da53da5cf6

--- mini_emulator_common.mk
+++ mini_emulator_common.mk
@@ -45,5 +45,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libeffects/data/audio_effects.conf:system/etc/audio_effects.conf \
     hardware/libhardware_legacy/audio/audio_policy.conf:system/etc/audio_policy.conf \
     device/generic/goldfish/camera/media_profiles.xml:system/etc/media_profiles.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     device/generic/goldfish/camera/media_codecs.xml:system/etc/media_codecs.xml \
     device/generic/goldfish/fstab.goldfish:root/fstab.goldfish
