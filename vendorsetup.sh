2878ca95fedf15fd813cc4f433f4083e17005af7
leozwang <leozwang@google.com>
2014-06-18 13:31:17 -0700

    Change product name to reduce fingerprint size.
    Bug: 15708300
    Change-Id: I05eab536a76873ed731015bb0c061ab1af4cd4f2

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,4 +20,4 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo mini_emulator_armv7a_neon-userdebug
+add_lunch_combo mini_emu_armv7a-userdebug
